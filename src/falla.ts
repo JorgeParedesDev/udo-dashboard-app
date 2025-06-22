d197b524fdd20ab5a545369ae1043c7379dcfb9a
Jorge Luis Paredes <jorge.paredes@alten.es>
2025-06-22 16:20:24 +0200

    final test

--- src/falla.ts
+++ src/falla.ts
@@ -1,4 +1,1 @@
-// Error forzado de tipo real
-const sumar = (a: number, b: number): number => a + b;
-
-const resultado = sumar(2, '3'); // ❌ string en vez de number
+const error: number = 'esto es un string'; // ❌ error explícito
